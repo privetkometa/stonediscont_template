29c2b7393888c4cfc9219fd066ae14a0ab558bdc
Ксения Губа <kseniaguba@192.168.1.105>
2020-01-10 01:08:12 +0300

    init

---
+++ app/js/common.js
@@ -0,0 +1,32 @@
+$(function() {
+  // calculate disconts at card
+  $(document).on('msoptionsprice_product_action', function (e, action, form, r) {
+      var pageId = document.getElementById('pageId').value;
+      if (action == 'modification/get' && r.success && r.data) {
+        if (r.data.rid == pageId) {
+          var modification = r.data.modification || {};
+          var discount = modification.options.discount || 0;
+          if (discount) {
+            var oldPrice = Math.round(modification.cost / ((100 - discount) / 100));
+            var formattedPrice = oldPrice.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+            document.getElementById('old_price').textContent = formattedPrice;
+            var benefit = oldPrice - modification.cost;
+            var formattedBenefit = benefit.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+            $("#discount").html(`<div class="product-card__discount">Скидка <span class="product-card__discount-value">${discount} %</span></div>`)
+            $("#benefit").html(`<span class="product-card__benefit-title">Экономия</span><span id="benefit-value" class="product-card__benefit-value">${formattedBenefit} ₽</span>`);
+          } else {
+            document.getElementById('benefit').textContent = '';
+            document.getElementById('discount').textContent = '';
+            document.getElementById('old_price').textContent = '';
+          }
+        }
+      }
+  });
+
+  $('.similar-carusel').owlCarousel({
+    items: 4,
+    margin: 30,
+    nav: true,
+  })
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+const gulp 					= require('gulp'),
+		gutil 				= require('gulp-util'),
+		sass 				= require('gulp-sass'),
+		sassGlob 			= require('gulp-sass-glob'),
+		concat        		= require('gulp-concat'),
+		uglify        		= require('gulp-uglify-es').default,
+		cleanCSS      		= require('gulp-clean-css'),
+		rename        		= require('gulp-rename'),
+		autoprefixer  		= require('gulp-autoprefixer'),
+		notify        		= require('gulp-notify'),
+		svgstore 			= require('gulp-svgstore'),
+		inject 				= require('gulp-inject'),
+		babel 				= require('gulp-babel'),
+		svgmin 				= require('gulp-svgmin');
+		ftp 				= require('gulp-ftp');
+
+sass.compiler = require('node-sass');
+
+function commonJs() {
+	return gulp.src('app/js/*.js')
+	.pipe(concat('common.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('public/js'))
+	.pipe(livereload());
+}
+
+function vendorJs() {
+	return gulp.src([
+		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/popper.js/dist/umd/popper.min.js',
+		'node_modules/bootstrap/dist/js/bootstrap.min.js',
+		'node_modules/jquery-modal/jquery.modal.min.js',
+		'node_modules/owl.carousel2/dist/owl.carousel.min.js',
+		])
+	.pipe(concat('vendor.min.js'))
+	// .pipe(babel({ presets: ['babel-preset-env'] }))
+	.pipe(uglify())
+	.pipe(gulp.dest('public/js'))
+	.pipe(ftp({
+		host: 'vh166.timeweb.ru',
+		user: 'cu67168_stonede',
+		pass: 'cxtEfVF9',
+		remotePath: '/public_html/assets/templates/frankenstein/public/js'
+	}))
+	.pipe(gutil.noop());
+}
+
+function css() {
+	return gulp.src('app/scss/*.scss')
+	.pipe(sassGlob())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('public/css'))
+	.pipe(ftp({
+		host: 'vh166.timeweb.ru',
+		user: 'cu67168_stonede',
+		pass: 'cxtEfVF9',
+		remotePath: '/public_html/assets/templates/frankenstein/public/css'
+	}))
+	.pipe(gutil.noop());
+}
+
+function svgSprite() {
+	return gulp.src('app/assets/icons/*.svg')
+	.pipe(svgmin({
+		plugins: [{
+			cleanupIDs: {
+					minify: true
+			}
+		},
+		{
+			removeUselessStrokeAndFill: {
+				stroke: true,
+				fill: true,
+				removeNone: true,
+				hasStyleOrScript: true,
+			}
+		}]
+	}))
+	.pipe(svgstore({ inlineSvg: true }))
+	.pipe(gulp.dest('public/assets/images/svg'))
+	.pipe(ftp({
+		host: 'vh166.timeweb.ru',
+		user: 'cu67168_stonede',
+		pass: 'cxtEfVF9',
+		remotePath: '/public_html/assets/templates/frankenstein/public/assets/images/svg'
+	}))
+	.pipe(gutil.noop());
+}
+
+function fonts() {
+	return gulp.src('app/assets/fonts/**/*.**')
+	.pipe(gulp.dest('public/assets/fonts'));
+}
+
+function images() {
+	return gulp.src('app/assets/images/*.**')
+	.pipe(gulp.dest('public/assets/images'))
+	.pipe(ftp({
+		host: 'vh166.timeweb.ru',
+		user: 'cu67168_stonede',
+		pass: 'cxtEfVF9',
+		remotePath: '/public_html/assets/templates/frankenstein/public/assets/images'
+	}))
+	.pipe(gutil.noop());
+}
+
+function html() {
+	return gulp.src(['app/html/**/*.html'])
+	.pipe(gulp.dest('public'))
+	.pipe(ftp({
+		host: 'vh166.timeweb.ru',
+		user: 'cu67168_stonede',
+		pass: 'cxtEfVF9',
+		remotePath: '/public_html/assets/templates/frankenstein/public'
+	}))
+	.pipe(gutil.noop());
+}
+
+exports.watch = function() {
+	livereload.listen();
+	gulp.watch('app/scss/**/*.scss', css);
+	gulp.watch('app/js/*.js', commonJs);
+	gulp.watch('app/html/**/*.html', html);
+	gulp.watch('app/assets/icons/*.svg', svgSprite);
+	gulp.watch('app/assets/images/*.**', images);
+}
+
+exports.default = gulp.series(
+	html,
+	gulp.parallel(
+		css,
+		commonJs,
+		vendorJs,
+		svgSprite,
+		fonts,
+		images,
+	)
+)
